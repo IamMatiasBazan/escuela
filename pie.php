997762c17bfaf27a152c28b94bdc086e0b4ff775
[DMatias <[Dbazanmatias2004@gmail.com>
2023-04-10 14:16:12 -0300

    Subiendo proyecto

---
+++ pie.php
@@ -0,0 +1,14 @@
+<footer class="px-2 py-2 fixed-bottom bg-dark">
+    <span class="text-muted">Simple proyecto de sistema escolar creado por
+        <a class="text-white" href="//parzibyte.me/blog">Matias Bazan</a>
+        &nbsp;|&nbsp;
+        <a target="_blank" class="text-white" href="https://github.com/parzibyte/escuela_php">
+            Ver código fuente
+        </a>
+    </span>
+</footer>
+</main>
+
+</body>
+
+</html>
